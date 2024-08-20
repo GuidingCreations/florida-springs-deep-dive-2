7b24444abcde9148a32644eba7ec1f77961f6a54
hmccarthy1 <huntermccarthy56@hotmail.com>
2024-08-19 20:37:05 -0500

    render account page as server component to avoid cookies error

--- app/(root)/account/page.tsx
+++ app/(root)/account/page.tsx
@@ -1,29 +1,9 @@
-'use server'
-
 import React from 'react'
-import { getLoggedInUser, getUserProfilePicture } from '@/lib/actions/user.action'
-import Image from 'next/image';
+import AccountPage from '@/components/AccountPage'
 
-const page = async () => {
-const userPic = await getUserProfilePicture();
-const user = await getLoggedInUser();
-
-return (
-    <div className='page-wrapper'>
-        <h1 className='center-self font-bold'>{user.firstName + " " + user.lastName}</h1>
-    <div className='flex items-center justify-center w-100'>
-    <div className=' profile-pic-wrapper'>
-        <Image 
-        src={userPic}
-        height={200}
-        width={200}
-        alt='Profile Picture'
-        className='profile-pic'
-        />
-    </div>
-    </div>
-    </div>
-
+const page = () => {
+  return (
+    <AccountPage />
   )
 }
 
---
+++ components/AccountPage.tsx
@@ -0,0 +1,30 @@
+'use server'
+
+import React from 'react'
+import { getLoggedInUser, getUserProfilePicture } from '@/lib/actions/user.action'
+import Image from 'next/image';
+
+const AccountPage = async () => {
+const userPic = await getUserProfilePicture();
+const user = await getLoggedInUser();
+
+return (
+    <div className='page-wrapper'>
+        <h1 className='center-self font-bold'>{user.firstName + " " + user.lastName}</h1>
+    <div className='flex items-center justify-center w-100'>
+    <div className=' profile-pic-wrapper'>
+        <Image 
+        src={userPic}
+        height={200}
+        width={200}
+        alt='Profile Picture'
+        className='profile-pic'
+        />
+    </div>
+    </div>
+    </div>
+
+  )
+}
+
+export default AccountPage
